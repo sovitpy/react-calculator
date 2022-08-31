d97a9566058bceb8d26e6140a1fb6693ae767a46
sovitpy <sovitlekhak@gmail.com>
2022-08-30 20:01:35 -0500

    feat: added HTML/JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+const createDigits = () => {
+  const digits = [];
+  for (let i = 0; i < 10; i++) {
+    digits.push(i);
+  }
+  return digits;
+};
 function App() {
+  const digits = createDigits();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="fi-calculator">
+        <div className="fi-screen">
+          <span>(0)</span>0
+        </div>
+        <div className="fi-operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          <button>DEL</button>
+        </div>
+        <div className="fi-digits">
+          {digits.map((e) => (
+            <button key={e.toString()}>{e}</button>
+          ))}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
